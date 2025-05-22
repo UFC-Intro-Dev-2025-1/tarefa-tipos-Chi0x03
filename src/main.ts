038cfe224a468acb83c6fe39274b128b5943e4b9
Caio Wagner <caiowagner@alu.ufc.br>
2025-05-22 20:00:51 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-// Escreva seu código aqui!
+interface Sala {
+  nome: string;
+  bloco: number;
+  quantidadeMaxDeAlunos: number;
+  possuiComputadores: boolean;
+  quantidadeDeComputadores: number | undefined
+  aberta: boolean;
+};
+
+const minhaSala: Sala = {
+  nome: 'Lab de Informática',
+  bloco: 1,
+  quantidadeMaxDeAlunos: 42,
+  possuiComputadores: true,
+  quantidadeDeComputadores: undefined,
+  aberta: true,
+};
+
+console.log(minhaSala);
